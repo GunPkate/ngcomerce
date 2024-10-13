67c58c2dbe3d096764ba4bda49a1ae7174be09da
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-10-13 18:16:00 +0700

    new page selectitem and order

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { MainPageModule } from './pages/p01_main/mainpage.module';
 import { MainPageComponent } from './pages/p01_main/mainpage.component';
 import { ItemlistComponent } from './pages/p02_itemlist/itemlist.component';
 import { itemlistPageModule } from './pages/p02_itemlist/itemlistPage.module';
+import { OrderComponent } from './pages/p03_order/order/order.component';
+import { SelectitemComponent } from './pages/p04_selectitem/selectitem/selectitem.component';
 
 
 const routes: Routes = [
   {path: '', component: MainPageComponent  },
   {path: 'itemlist', component: ItemlistComponent  },
+  {path: 'order', component: OrderComponent  },
+  {path: 'select', component: SelectitemComponent  },
   {path: '', redirectTo: '', pathMatch: 'full' },
 //   {path: 'category', lo: <div>1234<div/> },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/c01_navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
+import { OrderComponent } from './pages/p03_order/order/order.component';
+import { SelectitemComponent } from './pages/p04_selectitem/selectitem/selectitem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    OrderComponent,
+    SelectitemComponent,
   ],
   imports: [
     BrowserModule,
