670328f7a6b9485269dc466b96aebb7c0e6fe993
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-10-15 10:36:36 +0700

    order layout v1 main pic and desc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/c01_navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
-import { OrderComponent } from './pages/p03_order/order/order.component';
-import { MyCartComponent } from './pages/p04_mycart/mycart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    OrderComponent,
-    MyCartComponent,
   ],
   imports: [
     BrowserModule,
