fd1fb71b3da4089594440d0ccce87d68b7e38c07
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-10-15 14:38:46 +0700

    set page limit in img order

--- src/app/pages/p03_order/order/order.component.ts
+++ src/app/pages/p03_order/order/order.component.ts
@@ -11,7 +11,9 @@ export class OrderComponent implements OnInit {
 
   constructor(private router:Router) { }
   title: string = "My Order"
-  imgList = [
+  imgList:any = []
+  page: number = 1
+  imgAll = [
     {name:"a"},
     {name:"b"},
     {name:"c"},
@@ -24,9 +26,26 @@ export class OrderComponent implements OnInit {
     if( a[2] > 3){
       this.title = a[2] 
     }
+    this.changeImg(0);
   }
   
   changeImg(value: number){
-
+    this.page += value
+    this.imgList = [];
+    let limitMax = 3 * this.page;
+    let limitMin = limitMax - 3 +1;
+    if(this.imgAll.length >= limitMax){
+      for (let i = 0; i < limitMax; i++) {
+        this.imgList.push(this.imgAll[i])
+      }
+    }else{
+      for (let i = 0 + limitMin - 1; i < limitMax; i++) {
+        if(this.imgAll.length - 1){
+          this.imgList.push(this.imgAll[i])
+        }else{
+          this.imgList.push({name:""})
+        }
+      }
+    }
   }
 }
